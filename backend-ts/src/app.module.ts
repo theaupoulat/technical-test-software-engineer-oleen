3d18073e8b421a10237946cf0cbb82bfe6e8c5e7
Etienne Batise <etienne.batise@gmail.com>
2023-06-03 19:08:20 +0200

    backend-ts

--- backend-ts/src/app.module.ts
+++ backend-ts/src/app.module.ts
@@ -1,10 +1,32 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { DataSource } from 'typeorm'
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+
+import { User } from './user/entities/user.entity'
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'password',
+      database: 'backend-ts',
+      entities: [User],
+      synchronize: true,
+      autoLoadEntities: true
+    }),
+    UserModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private dataSource: DataSource) {}
+}
