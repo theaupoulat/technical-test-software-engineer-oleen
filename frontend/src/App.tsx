d03a15a9c1bcfadddb031f3d3e4c193edd71a815
Theau Poulat <theau.poulat@gmail.com>
2023-06-08 23:06:07 +0200

    feat/implement-front

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,20 @@
 import React from 'react'
-import UserList from './Components/Users'
-
+import { SlotsTable } from './Components/SlotsTable'
+import './index.css'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <UserList />
-      </header>
+    <div className="flex align-middle justify-items-center h-full w-full absolute  font-sans  text-neutral-1">
+      <div className=" m-auto w-3/5 mt-52 bg-white rounded-lg py-8 px-16">
+        <p className="text-3xl font-extrabold">Prenez rendez-vous</p>
+        <p>
+          Choisissez une heure de rendez-vous pour echanger par telephone avec
+          un expert.
+          <br />
+          L&apos;echange durera
+          <span className="font-bold"> 30 minutes.</span>
+        </p>
+        <SlotsTable />
+      </div>
     </div>
   )
 }
